32ce1a29fc294cf839247ec43e5a928a4263772b
Miguel Blanque <miguelblanque@gmail.com>
2022-08-28 17:27:24 +0200

    Creacion de Formulario

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import {Container , } from 'react-bootstrap'
-
+import {Container } from 'react-bootstrap'
+import Formulario from './components/Formulario'
 function App() {
 
 
@@ -8,6 +8,10 @@ function App() {
     <header className="py-5">
     <h1>Buscador de Bebidas</h1>
     </header>
+
+    <Container className='mt-5'>
+         <Formulario/>
+    </Container>
    </>
   )
 }
